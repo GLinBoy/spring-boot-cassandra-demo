be743d614a62cb0131ebab1a3d7359fe2ed1ea9b
GLinBoy <glinboy@live.com>
2024-03-11 20:07:30 +0100

    🕯 START: Initiated the project
    resolved #1

---
+++ src/test/kotlin/com/glinboy/demo/cassandra/CassandraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.demo.cassandra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CassandraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
